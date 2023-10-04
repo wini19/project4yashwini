f0f32e1896593c11c73860d295d6946945807908
wini19 <29128282+wini19@users.noreply.github.com>
2023-10-04 21:53:50 +0000

    Completed upto task 7 - kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=wini19/projectapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run projectapp --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward projectapp 8000:80
 
