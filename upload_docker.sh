9002bac448153e385d48cb70c03f321da629fc8f
wini19 <29128282+wini19@users.noreply.github.com>
2023-10-04 20:41:35 +0000

    completed upto task 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=wini19/projectapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username wini19
+docker tag projectapp $dockerpath
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
